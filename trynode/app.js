ba03f667f58a5cd770277dfc005cdab53458a68b
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-23 16:52:07 +0200

    change rquire to import

--- trynode/app.js
+++
@@ -1,8 +0,0 @@
-// const fs = require("fs");
-//
-// fs.writeFile("test.txt", "Witaj świecie!", () => {
-//     fs.readFile("test.txt", "utf8", (err, msg) => {
-//         console.log(msg)
-//     });
-// });
-
