01eb64d7c9b78b3787a07f0824fed18f98a44262
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-23 17:17:20 +0200

    Add another method to url C

--- node-server/server.mjs
+++ node-server/server.mjs
@@ -7,6 +7,16 @@ const server = http.createServer((req, res) => {
         res.end("Witaj na trasie A!");
     } else if (req.url === "/b") {
         res.end("Witaj na trasie B!");
+    } else if (req.url === "/c" && req.method === "POST") {
+        let body = [];
+        req.on("data", (chunk) => {
+            body.push(chunk)
+        });
+        req.on("end", () => {
+            const params = Buffer.concat(body);
+            console.log('treść', params.toString());
+            res.end(`Przesłane parametry: ${params.toString()}`);
+        })
     } else {
         res.end("Do zobaczenia!");
     }
