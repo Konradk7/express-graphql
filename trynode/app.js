8f707715c960b781ebbc81bdbc7ae64e5886db31
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-23 16:45:15 +0200

    Add first node script

---
+++ trynode/app.js
@@ -0,0 +1,8 @@
+// const fs = require("fs");
+//
+// fs.writeFile("test.txt", "Witaj świecie!", () => {
+//     fs.readFile("test.txt", "utf8", (err, msg) => {
+//         console.log(msg)
+//     });
+// });
+
