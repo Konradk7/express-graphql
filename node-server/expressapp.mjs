d74d2f0dce3f4d9738d24efeab86ac543c0911a6
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-25 21:30:19 +0200

    Add router to express

--- node-server/expressapp.mjs
+++ node-server/expressapp.mjs
@@ -1,11 +1,23 @@
 import express from "express";
 
 const app = express();
+const router = express.Router();
 
 app.use((req, res, next) => {
     console.log("Server node express został uruchomiony!")
     next();
-})
+});
 app.use((req, res, next) => {
     res.send("witaj, świecie! jestem oprogramowaniem pośrednim")
+});
+app.use(router);
+router.get("/a", (req, res, next) => {
+    res.send("Cześć! witaj na trasie A");
+});
+router.post("c", (req, res, next) => {
+    res.send("Cześć! witaj na trasie C")
+})
+
+app.listen({port: 8000}, () => {
+    console.log("Serwer node express został uruchomiony")
 })
