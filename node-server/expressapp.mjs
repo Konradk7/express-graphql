1aab289e065ca871f3508329f7203fc3a3ae9316
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-25 21:39:35 +0200

    add bodyparser

--- node-server/expressapp.mjs
+++ node-server/expressapp.mjs
@@ -1,15 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json())
 app.use((req, res, next) => {
     console.log("Server node express został uruchomiony!")
     next();
 });
-app.use((req, res, next) => {
-    res.send("witaj, świecie! jestem oprogramowaniem pośrednim")
-});
+// app.use((req, res, next) => {
+//     res.send("witaj, świecie! jestem oprogramowaniem pośrednim")
+// });
 app.use(router);
 router.get("/a", (req, res, next) => {
     res.send("Cześć! witaj na trasie A");
