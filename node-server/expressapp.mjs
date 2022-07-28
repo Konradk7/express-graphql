29ede070cac037a419117eb82ac73354fa3eb5fd
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-28 19:42:44 +0200

    change uri express server

--- node-server/expressapp.mjs
+++ node-server/expressapp.mjs
@@ -6,15 +6,28 @@ const router = express.Router();
 
 app.use(bodyParser.json())
 app.use((req, res, next) => {
-    console.log("Server node express został uruchomiony!")
-    throw new Error("Przykro nam - awaria!")
+    console.log("Pierwsze pośrednie oprogramowanie!")
+    next();
 });
 // app.use((req, res, next) => {
 //     res.send("witaj, świecie! jestem oprogramowaniem pośrednim")
 // });
 app.use(router);
-router.get("/a", (req, res, next) => {
-    res.send("Cześć! witaj na trasie A");
+router.get("/api/v1/users", (req, res, next) => {
+    const users = [
+        {
+            id: 1,
+            username: "Tomek",
+        },
+        {
+            id: 2,
+            username: "Janek",
+        },
+        {
+            id: 3,
+            username: "Lidia",
+        },
+    ];
 });
 router.post("c", (req, res, next) => {
     res.send(`Cześć! witaj na trasie C. Mam wiadomośćL ${req.body.message}`)
