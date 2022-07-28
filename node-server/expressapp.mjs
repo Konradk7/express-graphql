ffddae79c7a13426d1156100d7a17814a0418a81
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-28 20:04:19 +0200

    Add groups render

--- node-server/expressapp.mjs
+++ node-server/expressapp.mjs
@@ -1,41 +1,59 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = express();
 const router = express.Router();
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-    console.log("Pierwsze pośrednie oprogramowanie!")
+    console.log("First middleware.");
     next();
 });
-// app.use((req, res, next) => {
-//     res.send("witaj, świecie! jestem oprogramowaniem pośrednim")
-// });
+
 app.use(router);
 router.get("/api/v1/users", (req, res, next) => {
     const users = [
         {
             id: 1,
-            username: "Tomek",
+            username: "tom",
         },
         {
             id: 2,
-            username: "Janek",
+            username: "jon",
         },
         {
             id: 3,
-            username: "Lidia",
+            username: "linda",
         },
     ];
+    console.log(req.query.userid);
+    const user = users.find((usr) => usr.id == req.query.userid);
+    res.send(`User ${user?.username}`);
 });
-router.post("c", (req, res, next) => {
-    res.send(`Cześć! witaj na trasie C. Mam wiadomośćL ${req.body.message}`)
+router.post("/api/v1/groups", (req, res, next) => {
+    const groups = [
+        {
+            id: 1,
+            groupname: "Admins",
+        },
+        {
+            id: 2,
+            groupname: "Users",
+        },
+        {
+            id: 3,
+            groupname: "Employees",
+        },
+    ];
+    const group = groups.find((grp) => grp.id == req.body.groupid);
+    res.send(`Group ${group.groupname}`);
 });
-app.use((err, req, res, next) => {
-    res.status(500).send(message);
-})
 
-app.listen({port: 8000}, () => {
-    console.log("Serwer node express został uruchomiony")
-})
+app.use((err, req, res, next) => {
+    res.status(500).send(err.message);
+});
+
+app.listen({ port: 8000 }, () => {
+    console.log("Express Node server has loaded!");
+});
