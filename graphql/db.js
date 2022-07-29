440fb11ec80e3035286ed246ee4fc25b64186aba
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-29 22:36:33 +0200

    Add tsc

---
+++ graphql/db.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var uuid_1 = require("uuid");
+exports.todos = [
+    {
+        id: uuid_1.v4(),
+        title: "Zadanie pierwsze",
+        description: "Opis pierwszego zadania",
+    },
+    {
+        id: uuid_1.v4(),
+        title: "Zadanie drugie",
+        description: "Opis drugiego zadania",
+    },
+    {
+        id: uuid_1.v4(),
+        title: "Zadanie trzecie",
+    },
+];
