0a2629c76cee5d20b0abc3f52c00913c026d6be6
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-29 22:21:38 +0200

    Add server http to apollo

--- graphql/server.ts
+++ graphql/server.ts
@@ -3,16 +3,20 @@ import { ApolloServer } from "apollo-server-express";
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
+import { createServer } from "http";
 
-
+const { PubSub } = require('apollo-server-express');
+const pubsub = new PubSub();
 const app = express();
 const schema = makeExecutableSchema({ typeDefs, resolvers});
 const apolloServer = new ApolloServer({
     schema,
-    context: ({ req, res}: any) => ({ req, res}),
+    context: ({ req, res}: any) => ({ req, res, pubsub }),
 });
 apolloServer.applyMiddleware({ app, cors: false });
-app.listen({ port: 8000}, () => {
-    console.log("Serwer GraphQL został uruchomiony!")
+const httpServer = createServer(app);
+apolloServer.installSubscriptionHandlers(httpServer);
+httpServer.listen({ port: 8000}, () => {
+    console.log("Serwer GraphQL został uruchomiony!" + apolloServer.graphqlPath)
 })
 
